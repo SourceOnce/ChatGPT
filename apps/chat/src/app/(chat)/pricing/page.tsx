08bcbdb606100c04ecdf82de011cbd9c7847c8a1
SourceOne <51945211+SourceOnce@users.noreply.github.com>
2023-05-24 16:36:06 +0800

    Update page.tsx

--- apps/chat/src/app/(chat)/pricing/page.tsx
+++ apps/chat/src/app/(chat)/pricing/page.tsx
@@ -43,25 +43,25 @@ const prices: Price[] = [
   {
     name: "Pro",
     price: {
-      monthly: 5,
-      quarterly: 999,
-      yearly: 999,
-    },
-    features: [
-      "无 GPT-3.5 问答,GPT-3.5 可在主站 https://chat.one-ai.world 免费使用", 
-      "40 次 GPT-4 问答",
-   ],
-  },
-  {
-    name: "Premium",
-    price: {
       monthly: 10,
       quarterly: 999,
       yearly: 999,
     },
     features: [
+      "无 GPT-3.5 问答,GPT-3.5 可在主站 https://chat.one-ai.world 免费使用", 
+      "20 次 GPT-4 问答",
+   ],
+  },
+  {
+    name: "Premium",
+    price: {
+      monthly: 20,
+      quarterly: 999,
+      yearly: 999,
+    },
+    features: [
       "无 GPT-3.5 问答,GPT-3.5 可在主站 https://chat.one-ai.world 免费使用",
-      " 100 次 GPT-4 问答"
+      " 50 次 GPT-4 问答"
     ],
   },
 ];
